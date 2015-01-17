b099486b6d4c3a7d53a619e226297fa7773b4178
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-17 08:16:44 +0100

    fix method names; add legacy test

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -3,30 +3,51 @@
 use Reroute\Router;
 use Reroute\Url\Flat;
 use Reroute\Url\Regex;
+use Reroute\Url\Legacy;
 
 class UrlTest extends PHPUnit_Framework_TestCase
 {
-    public function testGenerateUrl()
+    public function testGenerateFlatUrl()
     {
         $router = new Router;
-        $router->state('home', new Flat('/'), function() {});
+        $router->state('home', new Flat('/'), function () {});
         $url = $router->get('home')->url()->generate();
         $this->assertEquals('http://localhost/', $url);
     }
 
-    public function testGenerateUrlWithParameters()
+    public function testGenerateRegexUrl()
     {
         $router = new Router;
         $router->state(
             'params',
             new Regex("/(?'first'\w+)/(?'last'\w+)/"),
-            function($last, $first) {}
+            function ($last, $first) {}
         );
         $url = $router->get('params')->url()->generate([
             'first' => 'john',
             'last' => 'doe',
         ]);
         $this->assertEquals('http://localhost/john/doe/', $url);
+    }
+
+    public function testGenerateLegacyUrl()
+    {
+        $router = new Router;
+        $router->state(
+            'legacy',
+            new Legacy("/(%s:str)/(%d:num)/(%f:flt)/(%a:all)"),
+            function () {}
+        );
+        $url = $router->get('legacy')->url()->generate([
+            'str' => 'string',
+            'num' => '42',
+            'flt' => '3.14',
+            'all' => 'ev/ry/thing/',
+        ]);
+        $this->assertEquals(
+            'http://localhost/string/42/3.14/ev/ry/thing/',
+            $url
+        );
     }
 }
 
