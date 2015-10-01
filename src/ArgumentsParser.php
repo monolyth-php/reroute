e55f84cc9ee0d9358048304cbdab5bee8104356c
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-01 10:08:48 +0200

    helper

---
+++ src/ArgumentsParser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Reroute;
+
+use ReflectionMethod;
+use ReflectionFunction;
+
+class ArgumentsParser
+{
+    public function __construct(callable $call)
+    {
+        if (is_object($call) && method_exists($call, '__invoke')) {
+            $reflection = new ReflectionMethod($call, '__invoke');
+        } else {
+            $reflection = new ReflectionFunction($call);
+        }
+        $parameters = $reflection->getParameters();
+        $arguments = [];
+        foreach ($parameters as $value) {
+            $arguments[$value->name] = $value->name == 'VERB' ?
+                (isset($_SERVER['HTTP_METHOD']) ?
+                    $_SERVER['HTTP_METHOD'] :
+                    'GET') :
+                null;
+        }
+        $this->arguments = $arguments;
+    }
+
+    public function parse(array $args)
+    {
+        $remove = [];
+        while (false !== ($curr = each($args))) {
+            if (is_string($curr['key'])
+                && array_key_exists($curr['key'], $this->arguments)
+            ) {  
+                $this->arguments[$curr['key']] = $curr['value'];
+                $remove[] = $curr['key'];
+                $next = each($args);
+                $remove[] = $next['key'];
+            }
+        }
+        foreach ($remove as $key) {
+            unset($args[$key]);
+        }
+        // For remaining arguments, use the next available index:
+        array_walk($this->arguments, function (&$value) use (&$args) {
+            if (is_null($value) && $args) {
+                $value = array_shift($args);
+            }
+        });
+        // Remove unset arguments:
+        $args = [];
+        foreach ($this->arguments as $arg) {
+            if (isset($arg)) {
+                $args[] = $arg;
+            }
+        }
+        return $args;
+    }
+}
+
