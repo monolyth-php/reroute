13f72eb63dc48266abba6d62846c09d1d215f910
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-01 10:10:15 +0200

    update tests to reflect v2

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -13,9 +13,7 @@ class RouterTest extends PHPUnit_Framework_TestCase
     public function testResolveReturnsState()
     {
         $router = new Router;
-        $router->state('home', new Flat('/'), function () {
-            return "Hello world!";
-        });
+        $router->when('/')->then('Hello world');
         $state = $router->resolve('/');
         $this->assertInstanceOf('Reroute\State', $state);
     }
@@ -23,46 +21,38 @@ class RouterTest extends PHPUnit_Framework_TestCase
     public function testBasicRoute()
     {
         $router = new Router;
-        $router->state('home', new Flat('/'), function () {
-            return "Hello world!";
-        });
+        $router->when('/')->then('Hello world!');
         $state = $router->resolve('/');
-        $out = $state->run();
-        $this->assertEquals('Hello world!', $out);
+        $this->assertEquals('Hello world!', $state->run());
     }
 
     public function unnamedParameter()
     {
         $router = new Router;
-        $router->state('test', new Regex("/(\d+)/"), function ($id) {
+        $router->when("/(\d+)/")->then(function ($id) {
             return $id;
         });
         $state = $router->resolve('/1/');
-        $id = $state->run();
-        $this->assetEquals(1, $id);
+        $this->assetEquals(1, $state->run());
     }
 
     public function testNamedParameter()
     {
         $router = new Router;
-        $router->state('user', new Regex("/(?'id'\d+)/"), function ($id) {
+        $router->when("/(?'id'\d+)/")->then(function ($id) {
             return $id;
         });
         $state = $router->resolve('/1/');
-        $id = $state->run();
-        $this->assertEquals(1, $id);
+        $this->assertEquals(1, $state->run());
     }
 
     public function testParameterOrder()
     {
         $router = new Router;
-        $router->state(
-            'order',
-            new Regex("/(?'first'\w+)/(?'last'\w+)/"),
-            function ($last, $first) {
-                return compact('last', 'first');
-            }
-        );
+        $router->when("/(?'first'\w+)/(?'last'\w+)/")
+               ->then(function ($last, $first) {
+                    return compact('last', 'first');
+               });
         $state = $router->resolve('/john/doe/');
         extract($state->run());
         $this->assertEquals('john', $first);
@@ -72,13 +62,10 @@ class RouterTest extends PHPUnit_Framework_TestCase
     public function testVerbInRandomPlace()
     {
         $router = new Router;
-        $router->state(
-            'random',
-            new Regex("/(?'foo'\w+)/(\w+)/"),
-            function ($bar, $VERB, $foo) {
-                return compact('bar', 'VERB', 'foo');
-            }
-        );
+        $router->when("/(?'foo'\w+)/(\w+)/")
+               ->then(function ($bar, $VERB, $foo) {
+                    return compact('bar', 'VERB', 'foo');
+               });
         $state = $router->resolve('/foo/bar/');
         extract($state->run());
         $this->assertEquals('GET', $VERB);
@@ -89,7 +76,7 @@ class RouterTest extends PHPUnit_Framework_TestCase
     public function testIgnoreGetParameters()
     {
         $router = new Router;
-        $router->state('home', new Flat('/'), function () {});
+        $router->when('/')->then(function () {});
         $state = $router->resolve('/?foo=bar');
         $this->assertInstanceOf('Reroute\State', $state);
     }
@@ -107,16 +94,26 @@ class RouterTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('DomainException', $e);
     }
 
-    public function testRouteUnder()
+    public function testRouteNesting()
     {
         $router = new Router;
-        $router->under('/foo', function ($router) {
-            $router->state('bar', new Flat('/bar/'), function () {});
+        $router->when('/foo/')
+               ->when('/bar/')->then(function () {});
+        $state = $router->resolve('/foo/bar/');
+        $this->assertInstanceOf('Reroute\State', $state);
+    }
+
+    public function testRouteCallbackNesting()
+    {
+        $router = new Router;
+        $router->when('/foo/', function ($router) {
+            $router->when('/bar/')->then(function () {});
         });
         $state = $router->resolve('/foo/bar/');
         $this->assertInstanceOf('Reroute\State', $state);
     }
 
+    /*
     public function testRouteGroup()
     {
         $router = new Router;
@@ -138,17 +135,18 @@ class RouterTest extends PHPUnit_Framework_TestCase
         $state = $router->resolve('/test/');
         $this->assertEquals(['foo', 'bar'], $state->group());
     }
+    */
 
     public function testRouteHost()
     {
         $router = new Router;
-        $router->host('http://foo.com', function ($router) {
-            $router->state('foo', new Flat('/foo/'), function () {
+        $router->when('http://foo.com/', function ($router) {
+            $router->when('/foo/')->then(function () {
                 return 'foo';
             });
         });
-        $router->host('http://bar.com', function ($router) {
-            $router->state('bar', new Flat('/bar/'), function () {
+        $router->when('http://bar.com/', function ($router) {
+            $router->when('/bar/')->then(function () {
                 return 'bar';
             });
         });
@@ -162,18 +160,10 @@ class RouterTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(null, $state);
     }
 
-    public function testLegacy()
-    {
-        $router = new Router;
-        $router->state('legacy', new Legacy('/(%s:str)/'), function () {});
-        $state = $router->resolve('/somestring/');
-        $this->assertInstanceOf('Reroute\State', $state);
-    }
-
     public function testAngular()
     {
         $router = new Router;
-        $router->state('angular', new Angular('/:str/'), function () {});
+        $router->when('/:angular/')->then(function () {});
         $state = $router->resolve('/somestring/');
         $this->assertInstanceOf('Reroute\State', $state);
     }
@@ -181,7 +171,7 @@ class RouterTest extends PHPUnit_Framework_TestCase
     public function testBraces()
     {
         $router = new Router;
-        $router->state('angular', new Braces('/{str}/'), function () {});
+        $router->when('/{braces}/')->then(function () {});
         $state = $router->resolve('/somestring/');
         $this->assertInstanceOf('Reroute\State', $state);
     }
@@ -189,9 +179,27 @@ class RouterTest extends PHPUnit_Framework_TestCase
     public function testNomatchUrl()
     {
         $router = new Router;
-        $router->state('404', new Nomatch, function() {});
+        $router->state('404', null)->then(function() {});
         $state = $router->get('404');
         $this->assertInstanceOf('Reroute\State', $state);
+    }
+
+    public function testGenerate()
+    {
+        $router = new Router;
+        $router->state('test', "http://foo.com/(?'p1':\w+)/{p2}/:p3/")
+               ->then(function () {});
+        $url = $router->generate(
+            'test',
+            ['p1' => 'foo', 'p2' => 'bar', 'p3' => 'baz']
+        );
+        $this->assertEquals('http://foo.com/foo/bar/baz/', $url);
+        $_SERVER['HTTP_HOST'] = 'foo.com';
+        $url = $router->generate(
+            'test',
+            ['p1' => 'foo', 'p2' => 'bar', 'p3' => 'baz']
+        );
+        $this->assertEquals('/foo/bar/baz/', $url);
     }
 }
 
