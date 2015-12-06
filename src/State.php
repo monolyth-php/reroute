9eea11ccc7864af948feca85c93d10a7e09b62ae
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-06 09:45:24 +0100

    add some docs, but mostly move arguments parsing to the state, it is the only place it is used now

--- src/State.php
+++ src/State.php
@@ -3,15 +3,31 @@
 namespace Reroute;
 
 use Exception;
+use ReflectionMethod;
+use ReflectionFunction;
 use Psr\Http\Message\RequestInterface;
 use Zend\Diactoros\Response\EmptyResponse;
 
 class State
 {
+    /**
+     * @var array
+     * Hash of supported actions and their associated state callbacks for this
+     * state.
+     */
     private $actions;
-    private $arguments = [];
 
+    /**
+     * @var string
+     * The (preferably unique) name of this state.
+     */
     public $name;
+
+    /**
+     * @var Psr\Http\Message\RequestInterface
+     * The current request.
+     */
+    private $request;
 
     public function __construct($name, $state)
     {
@@ -29,9 +45,9 @@ class State
             return new EmptyResponse(405);
         }
         $call = $this->actions[$method];
+        $this->request = $request;
         do {
-            $parser = new ArgumentsParser($call);
-            $args = $parser->parse($arguments, $request);
+            $args = $this->parseArguments($call, $arguments);
             foreach ($args as &$value) {
                 if (is_string($value)
                     && $this->isHttpAction(substr($value, 1))
@@ -78,6 +94,60 @@ class State
             $action,
             ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']
         );
+    }
+
+    private function parseArguments(callable $call, array $matches)
+    {
+        if (is_object($call) && method_exists($call, '__invoke')) {
+            $reflection = new ReflectionMethod($call, '__invoke');
+        } else {
+            $reflection = new ReflectionFunction($call);
+        }
+        $parameters = $reflection->getParameters();
+        $arguments = [];
+        $request = 'Psr\Http\Message\RequestInterface';
+        foreach ($parameters as $value) {
+            if ($class = $value->getClass()
+                and $class->implementsInterface($request)
+            ) {
+                $arguments["RequestInterface"] = $this->request;
+            } elseif ($value->isCallable()) {
+                $arguments[$value->name] = isset($this->actions[$value->name]) ?
+                    $this->actions[$value->name] :
+                    new EmptyResponse(405);
+            } else {
+                $arguments[$value->name] = null;
+            }
+        }
+        $remove = [];
+        $args = isset($matches) ? $matches : [];
+        while (false !== ($curr = each($args))) {
+            if (is_string($curr['key'])
+                && array_key_exists($curr['key'], $arguments)
+            ) {
+                $arguments[$curr['key']] = $curr['value'];
+                $remove[] = $curr['key'];
+                $next = each($args);
+                $remove[] = $next['key'];
+            }
+        }
+        foreach ($remove as $key) {
+            unset($args[$key]);
+        }
+        // For remaining arguments, use the next available index:
+        array_walk($arguments, function (&$value) use (&$args) {
+            if (is_null($value) && $args) {
+                $value = array_shift($args);
+            }
+        });
+        // Remove unset arguments:
+        $args = [];
+        foreach ($arguments as $arg) {
+            if (isset($arg)) {
+                $args[] = $arg;
+            }
+        }
+        return $args;
     }
 }
 
